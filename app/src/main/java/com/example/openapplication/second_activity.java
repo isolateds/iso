5add93e93810bc703d773602dd1d7fe464b0e586
isolateds <1623818766@qq.com>
2022-04-19 22:02:51 +0800

    .

--- app/src/main/java/com/example/openapplication/second_activity.java
+++ app/src/main/java/com/example/openapplication/second_activity.java
@@ -18,10 +18,15 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.RenderNode;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -32,6 +37,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -39,22 +47,34 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.internal.http2.Http2Reader;
+
 public class second_activity extends AppCompatActivity {
 
 
     private ImageView picture;
     private Button chooseFromAlbum;
+    private Button submit;
+    private TextView textView;
+    String path = null;
+    String data = "123";
 
 
     public static final int CHOOSE_PHOTO=2;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        picture = (ImageView) findViewById(R.id.imageView4);
-        chooseFromAlbum = (Button) findViewById(R.id.button2);
-
-        chooseFromAlbum.setOnClickListener(view -> {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_second);
+            picture = (ImageView) findViewById(R.id.imageView4);
+            chooseFromAlbum = (Button) findViewById(R.id.button2);
+            textView = (TextView) findViewById(R.id.textView2);
+            chooseFromAlbum.setOnClickListener(view -> {
 
            if(ContextCompat.checkSelfPermission(second_activity.this,
                    Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
@@ -67,7 +87,75 @@ public class second_activity extends AppCompatActivity {
                openAlbum();
            }
         });
+
+        submit = (Button) findViewById(R.id.button3);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (path != null){
+                    String result = "{\"data\": \"0\"}";
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String img = path;
+                            String url = "https://www.baidu.com";
+                            try {
+                                data = uploadImage(url, img);
+                            } catch (IOException e) {
+                                Looper.prepare();
+                                e.printStackTrace();
+                                Looper.loop();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                            Message message = Message.obtain();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("data", result);
+                            addTrackHandler.sendMessage(message);
+                        }
+                    }).start();
+                }
+            }
+        });
     }
+
+    public String uploadImage(String url, String imagePath) throws IOException, JSONException {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        File file = new File(imagePath);
+        RequestBody image = RequestBody.create(MediaType.parse("image/png"),file);
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("file", imagePath, image)
+                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+        Response response = okHttpClient.newCall(request).execute();
+        String data = response.body().string();
+        textView.setText(data);
+        JSONObject jsonObject = new JSONObject(data);
+        return jsonObject.optString("image");
+    }
+
+    Handler addTrackHandler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message message) {
+            String result = "";
+            try {
+                result = message.getData().getString("data");
+                Toast.makeText(second_activity.this, "新增成功", Toast.LENGTH_SHORT).show();
+            }catch (Exception e){
+
+            }
+            Toast.makeText(second_activity.this, "调用成功"+result, Toast.LENGTH_SHORT).show();//测试弹框
+            return true;
+        }
+    });
+
+
 
     private void  openAlbum(){
         Intent intent=new Intent("android.intent.action.GET_CONTENT");
@@ -106,9 +194,7 @@ public class second_activity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
-        if (data == null){
-            return;
-        } else {
+        if (data != null){
             if (requestCode == CHOOSE_PHOTO){
                 handleImageOnKitKat(data);
             }
@@ -141,7 +227,7 @@ public class second_activity extends AppCompatActivity {
                 String id = docId.split(":")[1];
                 String selection = MediaStore.Images.Media._ID+"="+id;
                 imagePath =getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
-                displayImage(imagePath);
+//              displayImage(imagePath);
 
 
             }else if(TextUtils.equals(uri.getAuthority(),"com.android.providers.downloads.documents")){
@@ -200,6 +286,7 @@ public class second_activity extends AppCompatActivity {
 
     private void displayImage(String imagePath){
 
+        path = imagePath;
         if(imagePath!=null){
             Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
             picture.setImageBitmap(bitmap);
